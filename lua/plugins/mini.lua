592c8e7782104e63b32c969a8d8f1f0bb4c28213
Brendha Casaro <brendhameira9@gmail.com>
2025-08-23 21:26:43 -0300

    .

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,19 +6,4 @@ return {
 	{ "echasnovski/mini.move", version = "*", opts = {} },
 	{ "echasnovski/mini.pairs", version = "*", opts = {} },
 	{ "echasnovski/mini.splitjoin", version = "*", opts = { mappings = { toggle = "fS" } } },
-	{
-		"echasnovski/mini.files",
-		version = "*",
-		opts = {},
-		config = function()
-			vim.keymap.set("n", "<C-n>", function()
-				local buf = vim.api.nvim_buf_get_name(0)
-				if #buf > 0 and not StartsWith(buf, "/") then
-					require("mini.files").close()
-				else
-					require("mini.files").open(buf, false)
-				end
-			end)
-		end,
-	},
 }
