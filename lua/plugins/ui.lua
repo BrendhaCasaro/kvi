fc9c6f054348166aac9edd6e14c49d687e403e47
Brendha Casaro <brendhameira9@gmail.com>
2025-09-07 02:20:30 -0300

    mudei o tema, agr ta lindinho

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,39 +1,39 @@
 return {
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-	},
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+  },
 
-	-- Statusline
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		event = "VeryLazy",
-		opts = require("configs.lualine"),
-	},
+  -- Statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    opts = require("configs.lualine"),
+  },
 
-	-- Theme
-	{
-		"olimorris/onedarkpro.nvim",
-		priority = 1000, -- Ensure it loads first
-		config = function()
-			vim.cmd.colorscheme("onedark_dark")
-		end,
-	},
+  -- Theme
+  {
+    "Mofiqul/dracula.nvim",
+    priority = 1000, -- Ensure it loads first
+    config = function()
+      vim.cmd.colorscheme("dracula")
+    end,
+  },
 
-	-- Diagnostics
-	{
-		"folke/trouble.nvim",
-		opts = {
-			warn_no_results = false,
-		},
-		cmd = "Trouble",
-		keys = {
-			{
-				"<leader>q",
-				"<cmd>Trouble diagnostics toggle<cr>",
-				desc = "show diagnostics",
-			},
-		},
-	},
+  -- Diagnostics
+  {
+    "folke/trouble.nvim",
+    opts = {
+      warn_no_results = false,
+    },
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>q",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "show diagnostics",
+      },
+    },
+  },
 }
